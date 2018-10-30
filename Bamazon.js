89165baa74a5782efa0cd49ecab33e75df499337
Tim Westberg <timwestberg43@gmail.com>
2018-10-29 18:13:55 -0700

    added read function along with npm packages(Inquirer,console.table)

--- Bamazon.js
+++ Bamazon.js
@@ -1,6 +1,9 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+// This NPM Package display tables in console
+const cTable = require('console.table');
+
 // create the connection information for the sql database
 var connection = mysql.createConnection({
   host: "localhost",
@@ -9,7 +12,7 @@ var connection = mysql.createConnection({
   port: 3306,
 
   // Your username
-  user: "TimWestberg",
+  user: "Timwestberg",
 
   // Your password
   password: "Dulce1352",
@@ -20,5 +23,28 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   // run the start function after the connection is made to prompt the user
+  readProducts();
   start();
 });
+
+function start() {
+
+
+}
+
+
+function readProducts() {
+        console.log("Selecting all products...\n");
+
+        connection.query("SELECT * FROM products", function(err, res) {
+
+          if (err) throw err;
+
+          // Log all results of the SELECT statement
+        //   console.log(res.RowDataPacket);
+          
+console.table(res)
+
+          connection.end();
+        });
+      }
